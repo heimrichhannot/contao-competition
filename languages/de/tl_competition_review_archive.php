7048306e5d28e109b85bd1ec7c2555ff971fb24b
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-12-05 10:16:47 +0100

    fixed lcales

--- languages/de/tl_competition_review_archive.php
+++ languages/de/tl_competition_review_archive.php
@@ -8,9 +8,9 @@ $arrLang = &$GLOBALS['TL_LANG']['tl_competition_review_archive'];
 $arrLang['title'] 		= array('Titel', 'Der Titel des Bewertungs-Archivs');
 $arrLang['memberGroups'] = array('Mitgliedergruppen', 'Wählen Sie hier die Mitgliedergruppen aus, die einem Mitglied ermöglichen, in diesem Archiv Bewertungen zu erstellen. Um keine Einschränkung zu machen, lassen Sie dieses Feld bitte leer.');
 $arrLang['submissionArchives'] = array('Bewerbungsarchive', 'Wählen Sie hier die Bewerbungsarchive aus, deren Bewerbungen in diesem Archiv Bewertungen zu erhalten können. Um keine Einschränkung zu machen, lassen Sie dieses Feld bitte leer.');
-$arrLang['filesDir']					= array('Exportverzeichnis', 'Wählen Sie hier das Verzeichnis aus, dem die exportierten Dateien hinzugefügt werden sollen.');
-$arrLang['filesUseHomeDir']				= array('Benutzerverzeichnisse verwenden', 'Wählen Sie diese Option, wenn die exportierten Dateien vorrangig dem Benutzerverzeichnis hinzugefügt werden sollen. Hat das aktuell eingeloggte Mitglied kein Benutzerverzeichnis, wird das Exportverzeichnis genutzt.');
-$arrLang['filesUseProtectedHomeDir']	= array('Geschützte Benutzerverzeichnisse verwenden', 'Wählen Sie diese Option, wenn die exportierten Dateien vorrangig dem geschützten Benutzerverzeichnis, dann dem normalen und dann dem Exportverzeichnis hinzugefügt werden sollen.');
+$arrLang['filesDir']					= array('Uploadverzeichnis', 'Wählen Sie hier das Verzeichnis aus, dem die hochgeladenen Dateien hinzugefügt werden sollen.');
+$arrLang['filesUseHomeDir']				= array('Benutzerverzeichnisse verwenden', 'Wählen Sie diese Option, wenn die hochgeladenen Dateien vorrangig dem Benutzerverzeichnis hinzugefügt werden sollen. Hat das aktuell eingeloggte Mitglied kein Benutzerverzeichnis, wird das Uploadverzeichnis genutzt.');
+$arrLang['filesUseProtectedHomeDir']	= array('Geschützte Benutzerverzeichnisse verwenden', 'Wählen Sie diese Option, wenn die hochgeladenen Dateien vorrangig dem geschützten Benutzerverzeichnis, dann dem normalen und dann dem Uploadverzeichnis hinzugefügt werden sollen.');
 $arrLang['filesDirName']				= array('Archiv-Verzeichnisname', 'Geben Sie hier bei Bedarf einen Verzeichnisnamen ein, der im Exportverzeichnis erzeugt wird (bspw. "meinwettbewerb").');
 
 /**
